98049916399694f0a556d25f6edbeba7008cf0c0
Far4on <128631049+whoami-altoid@users.noreply.github.com>
2025-02-02 01:31:49 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Ejemplo de interactividad: Cambiar el color del borde al hacer clic en una tarjeta
-document.querySelectorAll('.item-wrapper').forEach(card => {
-  card.addEventListener('click', () => {
-    card.style.borderColor = '#40169C';
-  });
-});
